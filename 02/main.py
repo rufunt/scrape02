9e0cbdaead9e16d85232395bf83fed8649e6a237
rufunt <jazzoo@mail.ru>
2018-04-08 16:22:13 +0300

    Add loop for parsing data

--- 02/main.py
+++ 02/main.py
@@ -9,11 +9,24 @@ def get_html(url):
 	return r.text
 
 
+def get_data(html):
+	soup = BeautifulSoup(html, 'lxml')
+	news = soup.find_all('div', class_='sh_art')
 
+	for n in news:
+		title = n.find('h2').find('u').text.strip()
+		url = n.find('h2').find('a').get('href')
+		snippet = n.find('p').text.strip()
+		print(url)
+
+
+
+	#return news
 
 
 def main():
 	url = 'https://footballhd.ru/allnews/'
+	print(get_data(get_html(url)))
 	
 
 
