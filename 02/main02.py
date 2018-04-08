4dca6a1b76d9473a20d2f66f0b0d3638007f1736
rufunt <jazzoo@mail.ru>
2018-04-08 23:22:54 +0300

    Change app

--- 02/main02.py
+++ 02/main02.py
@@ -11,35 +11,32 @@ def get_html(url):
 
 def write_csv(data):
 	with open('news.csv', 'a') as f:
-	writer = csv.writer(f)
+		writer = csv.writer(f)
 
-	writer.writerow((data['title'], data['snipet'], data['url'] ))
+		writer.writerow((data['title'], data['snipet'], data['url'] ))
 		
 
 
 def get_data(html):
 	soup = BeautifulSoup(html, 'lxml')
-	news = soup.find_all('div', class_='sh_art')
+	articles = soup.find_all('div', class_='sh_art')
 
-	for n in news:
-		title = n.find('h2').find('u').text.strip()
+	for n in articles:
+		title = n.find('h2').find('u').text
 		url = n.find('h2').find('a').get('href')
-		snippet = n.find('p').text.strip()
+		snippet = n.find('p').text
 		
 		data = {'title': title, 'snipet': snipet, 'url': url}
 
+		
 
-
-	#return news
+		write_csv(data)
 
 
 def main():
 	url = 'https://footballhd.ru/allnews/'
-	print(get_data(get_html(url)))
+	get_data(get_html(url))
 	
-
-
-
 
 
 
